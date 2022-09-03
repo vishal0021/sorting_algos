1aefcd976f6efda69ca681b789977f72f08413ee
Vishal Sharma <sharma.vishal449@gmail.com>
2022-09-03 20:27:01 +0530

    sorting implemented

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -2,14 +2,37 @@
 
 using namespace std;
 
+/*
+find the minimum element in the array
+swap it with the first element of the unsorted array
+repeat till entire array is sorted.
+
+*/
+
 int main()
 {
 
     int arr[12] = {3,12,4,2,25,8,5,30,1,7};
+    int n = 10;
 
-    for(int i =0; i<10; i++)
+    int min = arr[0];
+    for(int i = 0; i< n-1; i++)
     {
-        cout<<arr[i]<<endl;
+        for (int j =i+1; j<n; j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    //display array
+
+     for(int k =0; k<n; k++)
+    {
+        cout<<arr[k]<<" ";
     }
     
     return 0;
