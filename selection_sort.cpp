953103c4aa8380a437a74b32174e43596dd3051f
Vishal Sharma <sharma.vishal449@gmail.com>
2022-09-03 15:11:11 +0530

    initial commit

---
+++ selection_sort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int arr[12] = {3,12,4,2,25,8,5,30,1,7};
+
+    for(int i =0; i<10; i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    
+    return 0;
+}
